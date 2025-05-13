13fc7256f894b6e68e193a9848117552b3ebc81b
ARBI <larbi.fathi007@gmail.com>
2025-05-13 19:22:53 +0100

    Add files via upload

---
+++ mainwindow.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+
+mainwindow = tk.Tk()
+
+mainwindow.title("My first GUI Project")
+mainwindow.geometry("500x300")
+mainwindow.resizable(False, False) # fixe les dimensions de la fenetre on poura pas changer les dimensions
+
+
+
+
+
+mainwindow.mainloop()
